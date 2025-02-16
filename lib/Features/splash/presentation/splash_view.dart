d66b20f87e0235ddfef3f1b37959a70e07e364dd
mohammedshanaef <mohammed.shaneaf.963@gmail.com>
2025-02-16 21:30:05 +0300

    Structuring the file and just create Splash View

---
+++ lib/Features/splash/presentation/splash_view.dart
@@ -0,0 +1,13 @@
+import 'package:doctor_app/Features/splash/presentation/widgets/splash_view_body.dart';
+import 'package:flutter/material.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SplashViewBody(),
+    );
+  }
+}
