3094ba1812f56134000d664b594292785cbda17f
mohammedshanaef <mohammed.shaneaf.963@gmail.com>
2025-02-16 21:46:16 +0300

    add it image folder and create app images file

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for hierarchySplash1
+  /// images/HierarchySplash1.svg
+  static const String hierarchySplash1 = "images/HierarchySplash1.svg";
+
+  /// Assets for hierarchySplash2
+  /// images/HierarchySplash2.svg
+  static const String hierarchySplash2 = "images/HierarchySplash2.svg";
+
+  /// Assets for logo
+  /// images/logo.svg
+  static const String logo = "images/logo.svg";
+}
+
